55310041fcf5174f3118e2da5f2996cad34b903a
Yash <yshukla@wpi.edu>
2020-09-25 22:18:00 -0400

    High Fidelity Envs

---
+++ Experiments_HF/5/TurtleBot_v0/__init__.py
@@ -0,0 +1,11 @@
+from gym.envs.registration import register
+
+register(
+    id='TurtleBot-v0',
+    entry_point='TurtleBot_v0.envs:TurtleBotV0Env',
+)
+
+register(
+    id='TurtleBot-v1',
+    entry_point='TurtleBot_v0.envs:TurtleBotV1Env',
+)
